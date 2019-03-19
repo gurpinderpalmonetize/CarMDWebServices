7700a756f87cd467438d92d7d239c3d975ed5087
gurpinderpalmonetize <gurpinderpal.singh@monetizesolutions.com>
2019-03-19 12:46:26 +0530

    small fix

--- DataAccessLayers/WebService/RecallCollection.cs
+++ DataAccessLayers/WebService/RecallCollection.cs
@@ -1,9 +1,4 @@
 ﻿using Metafuse3.BusinessObjects;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccessLayers.WebService
 {
